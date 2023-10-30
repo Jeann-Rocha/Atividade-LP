d4871f7089825641ca6dafbbf52033684398859c
Jeann-Rocha <jeannrochasilva@gmail.com>
2023-10-30 18:35:29 -0300

    Adicionando Prints

--- comercio.py
+++ comercio.py
@@ -7,7 +7,7 @@ class Tipo_Produto(Enum):
 
 
 class Produto:
-    def _init_(self, name: str, codigo: int, preco : float, tipo: Tipo_Produto) -> None:
+    def __init__(self, name: str, codigo: int, preco : float, tipo: Tipo_Produto) -> None:
         self.name = name
         self.codigo = codigo
         self.preco = preco
@@ -16,20 +16,20 @@ class Produto:
 
 # produto tipo 1
 class Celular(Produto):
-    def _init_(self, name: str, codigo: int, preco: float, tipo: Tipo_Produto) -> None:
-        super()._init_(name, codigo, preco, tipo)
+    def __init__(self, name: str, codigo: int, preco: float, tipo: Tipo_Produto) -> None:
+        super().__init__(name, codigo, preco, tipo)
 
 
 # produto tipo 2
 class Laptop(Produto):
-    def _init_(self, name: str, codigo: int, preco: float, tipo: Tipo_Produto) -> None:
-        super()._init_(name, codigo, preco, tipo)
+    def __init__(self, name: str, codigo: int, preco: float, tipo: Tipo_Produto) -> None:
+        super().__init__(name, codigo, preco, tipo)
 
 
 # produto tipo 3
 class Computador(Produto):
-    def _init_(self, name: str, codigo: int, preco: float, tipo: Tipo_Produto) -> None:
-        super()._init_(name, codigo, preco, tipo)
+    def __init__(self, name: str, codigo: int, preco: float, tipo: Tipo_Produto) -> None:
+        super().__init__(name, codigo, preco, tipo)
 
 
 class ExcecaoTransacaoVenda(Exception):
@@ -37,8 +37,8 @@ class ExcecaoTransacaoVenda(Exception):
     """
     mensagem = " não consta no estoque!"
 
-    def _init_(self, name: str) -> None:
-        super()._init_(name + self.mensagem)
+    def __init__(self, name: str) -> None:
+        super().__init__(name + self.mensagem)
 
 
 class ExcecaoTransacaoRetorno(Exception):
@@ -46,8 +46,8 @@ class ExcecaoTransacaoRetorno(Exception):
     """
     mensagem = " não foi vendido!"
 
-    def _init_(self, name: str) -> None:
-        super()._init_(name + self.mensagem)
+    def __init__(self, name: str) -> None:
+        super().__init__(name + self.mensagem)
 
 
 
@@ -64,12 +64,14 @@ class Inventario:
             raise ExcecaoTransacaoVenda(produto.name)
         else:
             self.produtos_vendidos.append(produto)
+            print(f"{produto.name} (código: {produto.codigo}) foi vendido por R$ {produto.preco:.2f}.")
 
     def retornar_produto(self, produto: Produto) -> None:
         """Função que retorna um produto vendido para o estoque, caso este tenha sido vendido.
         """
         if produto in self.produtos_vendidos:
             self.produtos.append(produto) # produto retornado
+            print(f"{produto.name} (código: {produto.codigo}) foi retornado (valor: R$ {produto.preco:.2f}).")
         else:
             raise ExcecaoTransacaoRetorno(produto.name)
 
@@ -77,6 +79,7 @@ class Inventario:
         """Função que repõe (aumenta a quantidade de) um produto no estoque.
         """
         self.produtos.append(produto) # produto reposto
+        print(f"{produto.name} (código: {produto.codigo}) foi reposto no estoque por R$ {produto.preco:.2f} (quantidade: {self.produtos.count(produto)}).")
 
     def listar_produtos(self) -> None:
         """Função que lista o estoque de produtos disponíveis.
@@ -101,5 +104,5 @@ Inventario().repor_produto(produto2)
 Inventario().repor_produto(produto3)
 Inventario().vender_produto(produto2)
 Inventario().vender_produto(produto1)
-Inventario().retornar_produto(produto3)
+Inventario().retornar_produto(produto1)
 Inventario().listar_produtos()
